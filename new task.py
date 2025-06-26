1afb8057a1643c2f83bde14840b015cbe3e1e21f
Shivang216 <Shivangjhunjhunwala21@gmail.com>
2025-06-27 01:03:34 +0530

    Add files via upload

---
+++ new task.py
@@ -0,0 +1,24 @@
+a=input('entre the first number: ')
+a=float(a)
+b=input('entre the second number: ')
+b=float(b)
+c=(a+b)
+print(f'The sum of {a} and {b} is {c}')
+d=(a-b)
+print(f'The difference of {a} and {b} is {d}')
+e=(a*b)
+print(f'The product of {a} and {b} is {e}')
+f=(a/b)
+print(f'The quotient of {a} and {b} is {f}')
+
+
+
+
+
+
+
+
+x=input('entre your first name: ')
+y=input('entre your last name: ')
+z= x + "" + y
+print("Hello,",x, y, "! Welcome to python program ")
